ee5633f9a1e3c8f0bfad5eff70f95d642b18b90e
Sushmashree PS <159642918+Sushmashreeps@users.noreply.github.com>
2024-07-13 22:59:23 +0530

    Add files via upload

---
+++ 7.web developmet(mvc) to access student data/src/main/java/ControllerServlet/ControllerServlet.java
@@ -0,0 +1,40 @@
+package ControllerServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import endterm7.LoginBean;
+
+
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("name");
+		String password=request.getParameter("password");
+		LoginBean bean=new LoginBean();
+		bean.setName(name);
+		bean.setPassword(password);
+		request.setAttribute("bean",bean);
+		boolean status=bean.validate();
+		if(status){
+		RequestDispatcher rd=request.getRequestDispatcher("login-success.jsp");
+		rd.forward(request,response);
+		}
+		else{
+		RequestDispatcher rd=request.getRequestDispatcher("login-error.jsp");
+		rd.forward(request,response);
+		}
+
+	}
+
+}
